5cd63e6311ec33e27da454c5e3f719608a4cf3aa
lubien <lubien1996@gmail.com>
2022-04-14 15:33:46 -0300

    Save

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Tarefas</h1>
+
+    <ul>
+      <li v-for="task in tasks" :key="task">
+        {{task}}
+
+        <button @click="deleteTask(task)">Apagar</button>
+      </li>
+    </ul>
+
+    <input type="text" v-model="newTask" @keypress.enter="addTask">
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      newTask: '',
+      tasks: ['Finalizar curso']
+    }
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.length === 0) { return }
+      this.tasks.push(this.newTask)
+      this.newTask = ''
+    },
+
+    deleteTask(task) {
+      this.tasks = this.tasks.filter(t => t !== task)
+    }
   }
 }
 </script>
